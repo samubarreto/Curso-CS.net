f627e7ec4a772970e7abd3d82906266e707a713a
Samuel Pereira <samu.barreto2004@gmail.com>
2024-05-03 16:12:21 -0300

    módulo 2 finalizado

---
+++ 2.Módulo/_0Desafios/_2Desafio/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class MinhaClasse
+{
+  static void Main(string[] args)
+  {
+    int N = int.Parse(Console.ReadLine());
+    var hours = N / 3600;
+    var minutes = (N / 60) % 60;
+    var seconds = N % 60;
+    Console.WriteLine($"{hours}:{minutes}:{seconds}");
+  }
+}
