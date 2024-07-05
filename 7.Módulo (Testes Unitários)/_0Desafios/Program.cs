9be0f29599ac40ac2926a0c73d329fbe79351deb
Samuel Pereira Barreto <samu.barreto2004@gmail.com>
2024-07-04 22:40:40 -0300

    finalizados desafios do módulo de testes unitários

---
+++ 7.Módulo (Testes Unitários)/_0Desafios/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    // // Desafio 1
+    // int c = 1;
+    // float nota1 = 0, nota2 = 0;
+
+    // while (c <= 2)
+    // {
+    //   float nota = float.Parse(Console.ReadLine());
+
+    //   if (nota >= 0 && nota <= 10)
+    //   {
+    //     if (c == 1)
+    //     {
+    //       nota1 = nota;
+    //     }
+    //     else if (c == 2)
+    //     {
+    //       nota2 = nota;
+    //     }
+    //     c++;
+    //   }
+    //   else
+    //   {
+    //     Console.WriteLine("nota invalida");
+    //   }
+    // }
+    // float media = (nota1 + nota2) / 2;
+    // Console.WriteLine($"media = {media:F2}");
+
+    // Desafio 2
+    // int N = int.Parse(Console.ReadLine());
+
+    // //Exibir "Ho" do papai noel
+    // for (int i = 0; i < N; i++)
+    // {
+    //   if (i + 1 != N)
+    //   {
+    //     Console.Write("Ho ");
+    //   }
+    //   else
+    //   {
+    //     Console.WriteLine("Ho!");
+    //   }
+    // }
+
+    // Desafio 3
+    // HashSet<string> joias = new HashSet<string>();
+    // string linha;
+
+    // while ((linha = Console.ReadLine()) != null)
+    // {
+    //   joias.Add(linha);
+    // }
+
+    // Console.WriteLine(joias.Count);
+  }
+}
